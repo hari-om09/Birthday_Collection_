b63d3f91df391901bd0efe58d7851fd3df4f3ab6
HARIOM KUMAR <162400134+hari-om09@users.noreply.github.com>
2025-03-31 15:02:05 +0530

    Update services.js

--- client/src/services.js
+++ client/src/services.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 
-  baseURL: 'http://localhost:3000', 
+  baseURL: 'https://birthday-collection.onrender.com', 
 
   headers: {
     'Content-Type': 'application/json',
